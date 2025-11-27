2875639ce3880656da3569d920faf34505e42224
yutacommits <you@example.com>
2025-11-27 05:17:49 +0000

    Pulled out variables and output:

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -136,17 +136,6 @@ resource "aws_security_group" "instance" {
   }
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
-
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
-
 terraform {
   backend "s3" {
     bucket         = "terraform-state-yuta"
@@ -155,5 +144,4 @@ terraform {
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
-
 }
